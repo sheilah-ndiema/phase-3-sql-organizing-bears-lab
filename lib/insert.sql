dcbb85edbb00a894c80fe3990ee23819c40a5696
Ndiema-S <you@example.com>
2022-10-31 08:53:00 +0300

    assignment omplete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, alive) VALUES ('Mr.Chocolate', 3, 'M', 1);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Rowdy', 4, 'F', 1);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Tabitha', 5, 'M',0);
+INSERT INTO bears (name, age, sex, alive ) VALUES ('Sergeant Brown', 6, 'F',0);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Melissa', 7, 'M',0);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Grinch', 8, 'F',1);
+INSERT INTO bears (name, age, sex, alive) VALUES ('Wendy', 9, 'F',1);
+INSERT INTO bears (name, age, sex, alive) VALUES (NULL, 10, 'M',0);
+
